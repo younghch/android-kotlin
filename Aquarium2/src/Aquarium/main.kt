e7d0349c23e66c6a157b79613fad2a3e3c59b9bb
Young <younghch42@gmail.com>
2022-04-06 16:31:51 +0900

    Update gitignore for android project

---
+++ Aquarium2/src/Aquarium/main.kt
@@ -0,0 +1,12 @@
+package Aquarium
+
+fun main (args: Array<String>) {
+    buildAquarium()
+}
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    println("Length: ${myAquarium.length}"+
+            "Width: ${myAquarium.width}"+
+            "Height: ${myAquarium.height}")
+}
