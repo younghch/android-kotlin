8de05412daa8f75d65f916c2dd112579d1680376
Young <younghch42@gmail.com>
2022-04-11 17:37:49 +0900

    Add Color my views app

---
+++ ColorMyViews/app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.colormyviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    //    https://developer.android.com/topic/libraries/view-binding/
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            binding.boxOneText,
+            binding.boxTwoText,
+            binding.boxThreeText,
+            binding.boxFourText,
+            binding.boxFiveText,
+            binding.redButton,
+            binding.greenButton,
+            binding.yellowButton
+        )
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+
+            R.id.red_button -> view.setBackgroundResource(R.color.red)
+            R.id.green_button -> view.setBackgroundResource(R.color.green)
+            R.id.yellow_button -> view.setBackgroundResource(R.color.yellow)
+
+            else -> view.setBackgroundColor(Color.CYAN)
+        }
+
+    }
+}
